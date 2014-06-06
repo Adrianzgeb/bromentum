49413f461540d9834ce29465010c10204aa5c13e
Adrian Zgeb <adrian.zgeb@mate1inc.com>
2014-06-06 21:11:09 +0000

    bla bla bla

--- getTest.js
+++
@@ -1,8 +0,0 @@
-var nano = require('nano')('https://admin:bravoechoechoromeo@bromentum.couchappy.com')
-, adrian = nano.use('adrian')
-
-adrian.get( '', {revs_info: true}, function(err, body) {
-    if (!err){
-        console.log(body);
-    }
-});
